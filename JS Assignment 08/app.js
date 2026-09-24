22eb713991c0672128565d32116884b1220d1f5b
M Ishaq Khan <ishaqkh2019@gmail.com>
2026-09-24 11:43:20 +0500

    Update app.js

--- JS Assignment 08/app.js
+++ JS Assignment 08/app.js
@@ -6,11 +6,11 @@ let searchInput = document.getElementById("searchInput");
 let tasks = [];
 
 
-// Add task
 addBtn.onclick = function () {
+
     let task = taskInput.value.trim();
 
-    if (task === "") {
+    if (task == "") {
         alert("Please enter a task");
         return;
     }
@@ -22,21 +22,20 @@ addBtn.onclick = function () {
 };
 
 
-// Add task with Enter
 taskInput.onkeydown = function (e) {
-    if (e.key === "Enter") {
+
+    if (e.key == "Enter") {
         addBtn.click();
     }
 };
 
 
-// Show tasks
 function showTasks() {
+
     taskList.innerHTML = "";
 
     let search = searchInput.value.toLowerCase();
 
-    // Loop
     for (let i = 0; i < tasks.length; i++) {
 
         if (!tasks[i].toLowerCase().includes(search)) {
@@ -46,48 +45,43 @@ function showTasks() {
         let li = document.createElement("li");
 
         let text = document.createElement("span");
-        text.className = "task-text";
         text.innerText = tasks[i];
+        text.className = "task-text";
 
-        // Complete
+        
         text.onclick = function () {
             text.classList.toggle("done");
         };
 
 
-        // Buttons
         let buttons = document.createElement("div");
         buttons.className = "buttons";
 
-
-        // Edit
         let edit = document.createElement("button");
         edit.className = "edit";
         edit.innerHTML = '<i class="fa-solid fa-pen"></i>';
 
         edit.onclick = function () {
+
             let newTask = prompt("Edit task:", tasks[i]);
 
-            if (newTask !== null && newTask.trim() !== "") {
+            if (newTask != null && newTask.trim() != "") {
                 tasks[i] = newTask.trim();
                 showTasks();
             }
         };
 
+        let deleteBtn = document.createElement("button");
+        deleteBtn.className = "delete";
+        deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
-        // Delete
-        let del = document.createElement("button");
-        del.className = "delete";
-        del.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-        del.onclick = function () {
+        deleteBtn.onclick = function () {
             tasks.splice(i, 1);
             showTasks();
         };
 
-
         buttons.appendChild(edit);
-        buttons.appendChild(del);
+        buttons.appendChild(deleteBtn);
 
         li.appendChild(text);
         li.appendChild(buttons);
@@ -96,10 +90,6 @@ function showTasks() {
     }
 }
 
-
-// Search
 searchInput.oninput = function () {
     showTasks();
 };
-
-
